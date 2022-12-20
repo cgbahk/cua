b5ffc4118306c63e9f01dbbe90697bd8d0a36a1e
Cheongyo Bahk <cg.bahk@gmail.com>
2022-12-20 18:34:41 +0900

    Extract out util

--- revisit_repo.py
+++ revisit_repo.py
@@ -6,19 +6,20 @@ from github import Github
 import pandas as pd
 
 
+def has_label(issue, label_name: str):
+    for label in issue.labels:
+        if label.name == label_name:
+            return True
+
+    return False
+
+
 @hydra.main(version_base="1.2", config_path=".", config_name=Path(__file__).stem)
 def main(cfg):
     g = Github(**cfg.session_info)
 
     repo = g.get_repo(cfg.repo)
     issues = list(repo.get_issues(state="all"))
-
-    def has_label(issue, label_name: str):
-        for label in issue.labels:
-            if label.name == label_name:
-                return True
-
-        return False
 
     if "label_to_exclude" in cfg:
         issues = [issue for issue in issues if not has_label(issue, cfg.label_to_exclude)]
